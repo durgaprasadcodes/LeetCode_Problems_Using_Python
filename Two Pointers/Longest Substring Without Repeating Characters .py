ed863e99cf33113e635c5287f6b86dc34efb6dd6
ANIL <durgaprasad04289@gmail.com>
2026-03-05 22:58:20 +0530

    Longest Substring Without Repeating

--- Two Pointers/Longest Substring Without Repeating Characters .py
+++ Two Pointers/Longest Substring Without Repeating Characters .py
@@ -6,16 +6,19 @@ class Solution(object):
         """
         if len(s)==1 or s ==" " :
             return 1
-        res=""
+        r=set()
         cur_len=0
         i=0
-        for j in range(1,len(s)):
-            if s[j]==s[i]:
-                cur_len=max(cur_len,j-i+1)
+        for j in range(len(s)):
+            while s[j] in r:
+                r.remove(s[i])
                 i+=1
-                print(s[i],s[j])
+            r.add(s[j])
+            cur_len=max(cur_len,j-i+1)
         return cur_len
+
+
         
         
 s=Solution()
-print(s.lengthOfLongestSubstring("anviaj"))
+print(s.lengthOfLongestSubstring("ohvhjdml"))
