d28198b552c1a2a55276423abdbab11e5286fc95
ANIL <durgaprasad04289@gmail.com>
2026-03-12 16:48:58 +0530

    Fruits Into Baskets

--- Two Pointers/Fruits Into Basket II.py
+++ Two Pointers/Fruits Into Basket II.py
@@ -5,4 +5,25 @@ class Solution(object):
         :type baskets: List[int]
         :rtype: int
         """
-        
+        n = len(fruits)
+        used = [False] * n
+        unplaced = 0
+
+        for fruit in fruits:
+            placed = False
+            for i, cap in enumerate(baskets):
+                if not used[i] and cap >= fruit:
+                    used[i] = True
+                    placed = True
+                    break
+            if not placed:
+                unplaced += 1
+
+        return unplaced
+    
+if __name__ == "__main__":
+    s = Solution()
+    # Expected: 1
+    print(s.numOfUnplacedFruits([4, 2, 5], [3, 5, 4]))
+    # Expected: 0
+    print(s.numOfUnplacedFruits([3, 6, 1], [6, 4, 7]))
