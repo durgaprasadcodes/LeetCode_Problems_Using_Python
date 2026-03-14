a5e7c1964a870dcd60d95e408371c148b2c2d7c3
ANIL <durgaprasad04289@gmail.com>
2026-03-14 13:50:43 +0530

    Find All Anagrams in a String

---
+++ Two Pointers/Find All Anagrams in a String.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        res=[]
+        j=0
+        p_freq=[0]*26
+        wind_freq=[0]*26
+        for i in p:
+            p_freq[ord(i)-97]+=1
+        for i in s[:len(p)]:
+            wind_freq[ord(i)-97]+=1
+        if p_freq==wind_freq:
+            res.append(j)
+        for i in range(len(p),len(s)):
+            wind_freq[ord(s[i-len(p)])-97]-=1
+            wind_freq[ord(s[i])-97]+=1
+            j+=1
+            if p_freq==wind_freq:
+                res.append(j)
+        return res
+
+    
+s=Solution()
+# print(s.findAnagrams("cbaebabacd","abc"))
+print(s.findAnagrams("abab","ab"))
