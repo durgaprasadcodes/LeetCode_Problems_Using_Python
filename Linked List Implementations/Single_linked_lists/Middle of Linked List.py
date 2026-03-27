695f21512c8a2be7c6005e23bb1461f4e6061a80
Durga Prasad Kota <durgaprasad04289@gmail.com>
2026-03-27 22:48:39 +0530

    Middle of the Linked List

--- Linked List Implementations/Single_linked_lists/Middle of Linked List.py
+++ Linked List Implementations/Single_linked_lists/Middle of Linked List.py
@@ -9,4 +9,10 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        
+        fast=head
+        slow=head
+        while fast and fast.next:
+            fast=fast.next.next
+            slow=slow.next
+
+        return slow
