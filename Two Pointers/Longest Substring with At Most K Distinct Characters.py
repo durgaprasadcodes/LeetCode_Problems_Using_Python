d2428c9880dfc1864e15a6014156094ff9e05d33
Durga Prasad Kota <durgaprasad04289@gmail.com>
2026-04-06 20:45:05 +0530

    Longest Substring with K Uniques (Medium)

---
+++ Two Pointers/Longest Substring with At Most K Distinct Characters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestKSubstr(self, s, k):
+        # code here
+        from collections import defaultdict
+        d=defaultdict(int)
+        l=0
+        max_len=float('-inf')
+        for r in range(len(s)):
+            d[s[r]]+=1
+            while len(d) > k:
+                d[s[l]]-=1
+                if d[s[l]]==0:
+                    del d[s[l]]
+                l+=1
+            if len(d)==k:
+                max_len=max(max_len,r-l+1)
+        return max_len if max_len!=float('-inf') else -1
