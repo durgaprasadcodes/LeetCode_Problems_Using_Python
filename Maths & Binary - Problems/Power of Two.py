f9200a24e683faee63036ff45dfb85d4665dc677
Durga Prasad Kota <durgaprasad04289@gmail.com>
2026-04-14 20:51:41 +0530

    Adding Complexities for Power Sum 2

--- Maths & Binary - Problems/Power of Two.py
+++ Maths & Binary - Problems/Power of Two.py
@@ -5,3 +5,8 @@ class Solution(object):
         :rtype: bool
         """
         return n & n-1==0 if n else False
+    
+Solution().isPowerOfTwo(16)
+
+# Time Complexity: O(1)
+# Space Complexity: O(1)
