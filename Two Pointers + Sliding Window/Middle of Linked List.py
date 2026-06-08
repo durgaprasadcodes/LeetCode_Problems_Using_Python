7bf6e9315a0b2680a45a8d5c4691a191b30760b3
Durga Prasad Kota <durgaprasad04289@gmail.com>
2026-06-08 14:02:14 +0530

    Middle of the Linked List

--- Two Pointers + Sliding Window/Middle of Linked List.py
+++ Two Pointers + Sliding Window/Middle of Linked List.py
@@ -1,18 +1,18 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
+class Solution:
     def middleNode(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        fast=head
-        slow=head
+        if not head:
+            return None
+
+        slow = head
+        fast = head
+
         while fast and fast.next:
-            fast=fast.next.next
-            slow=slow.next
+            slow = slow.next
+            fast = fast.next.next
 
         return slow
