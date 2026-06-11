33b91e53a2ab6c05cd49dab3fa99eeb600c8c575
Durga Prasad Kota <durgaprasad04289@gmail.com>
2026-06-11 18:53:03 +0530

    Remove Nth Node From End of List

--- Linked List Implementations/Single_linked_lists/Remove Nth Node From End.py
+++ Linked List Implementations/Single_linked_lists/Remove Nth Node From End.py
@@ -4,25 +4,20 @@ class ListNode(object):
         self.val = val
         self.next = next
 
-class Solution(object):
-    def removeNthFromEnd(self, head, n):
-        """
-        :type head: Optional[ListNode]
-        :type n: int
-        :rtype: Optional[ListNode]
-        """
-        dummy= ListNode(0)
-        dummy.next=head
-
-        s=f=dummy
-
-        while n:
+class Solution:
+    def removeNthFromEnd(self, head):
+        if not head:
+            return None
+            
+        dummy = ListNode(0,head)
+        f=s=dummy
+        for _ in range(n):
             f=f.next
-            n-=1
         
         while f and f.next:
-            s=s.next
             f=f.next
+            s=s.next
+
         s.next=s.next.next
         return dummy.next
 
