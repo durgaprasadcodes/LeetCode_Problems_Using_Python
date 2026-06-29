3f69ed989027891cf7337cdc7f49764dc538e8b2
Durga Prasad Kota <durgaprasad04289@gmail.com>
2026-06-29 10:57:41 +0530

    25 Reverse Nodes in k-Group (HARD)

---
+++ Linked List Implementations/Single_linked_lists/Reverse Nodes in k-Group(Hard).py
@@ -0,0 +1,45 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseKGroup(self, head,k):
+        if not head or not head.next:
+            return head
+
+        length = 0
+        curr = head
+        while curr:
+            length+=1
+            curr = curr.next
+
+        if length < k:
+            return head
+
+        dummy = ListNode(0,head)
+        prev  = dummy
+        curr = head
+
+        def reverse(head,k):
+            curr = head
+            prev = None
+            
+            while k:
+                nxt = curr.next
+                curr.next = prev
+                prev = curr
+                curr = nxt
+                k-=1
+            return prev , curr
+
+        for _ in range(int(length//k)):
+            cur_head,next_head = reverse(curr,k)
+
+            prev.next = cur_head
+            curr.next = next_head
+
+            prev = curr
+            curr = next_head
+
+        return dummy.next 
+
