e292eca1b65a25821e9e352e2d3390df4d4c3107
Durga Prasad Kota <durgaprasad04289@gmail.com>
2026-07-09 23:03:55 +0530

    2816 Double a Number Represented as a Linked List

---
+++ Linked List Implementations/Single_linked_lists/Double a Number Represented as a Linked List.py
@@ -0,0 +1,21 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def doubleIt(self, head):
+        if head.val > 4:
+            head = ListNode(0, head)
+
+        node = head
+
+        while node:
+            node.val = (node.val * 2) % 10
+
+            if node.next and node.next.val > 4:
+                node.val += 1
+
+            node = node.next
+
+        return head
