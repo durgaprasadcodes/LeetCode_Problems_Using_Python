fdcd090062fe884d7d7d252863d0b84de2c0dab5
Durga Prasad Kota <durgaprasad04289@gmail.com>
2026-07-13 20:31:41 +0530

    147. Insertion Sort List

--- Linked List Implementations/Single_linked_lists/Insertion Sort List.py
+++ Linked List Implementations/Single_linked_lists/Insertion Sort List.py
@@ -4,17 +4,22 @@ class ListNode:
         self.next = next
 class Solution:
     def insertionSortList(self, head):
-        
         if not head or not head.next:
             return head
-        dummy = ListNode(0)
-        curr = head
+        dummy = ListNode(0,head)
+        curr = head.next
+        tail = head
         while curr:
+            nxt_nodes = curr.next
+            if tail.val<=curr.val:
+                tail = curr
+                curr = nxt_nodes
+                continue
+            tail.next = curr.next
             prev = dummy
-            next_nodes = curr.next
             while prev.next and prev.next.val < curr.val:
                 prev = prev.next
             curr.next = prev.next
             prev.next = curr
-            curr = next_nodes
+            curr = nxt_nodes
         return dummy.next
