ab49fbfdb6b8378a2fbfa68761602e25ca181a3a
Durga Prasad Kota <durgaprasad04289@gmail.com>
2026-07-24 22:17:33 +0530

    1019 Next Greater Node In Linked List

--- Linked List Implementations/Single_linked_lists/Next Greater Node In Linked List.py
+++ Linked List Implementations/Single_linked_lists/Next Greater Node In Linked List.py
@@ -10,4 +10,25 @@ class Solution:
             while stk and arr[stk[-1]]<arr[i]:
                 ans[stk.pop()] = arr[i]
             stk.append(i)
-        return ans
+        return ans# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def nextLargerNodes(self, head):
+        
+        curr = head
+        arr = []
+        while curr:
+            arr.append(curr.val)
+            curr = curr.next
+        res = [0]*len(arr)
+        stack = []
+        for i in range(len(arr)):
+            while stack and arr[stack[-1]]<arr[i]:
+                idx = stack.pop()
+                res[idx] = arr[i]
+            stack.append(i)
+            
+        return res
